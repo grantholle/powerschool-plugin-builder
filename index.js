f33ffb741d9e124aaf4b3485d96741059670e632
Grant Holle <hollegrant@gmail.com>
2017-05-25 09:30:13 +0800

    Deleting previous zip before building

--- index.js
+++ index.js
@@ -41,65 +41,84 @@ if (!program.output) {
 const source = normalize(p.isAbsolute(program.source) ? program.source : p.join(process.cwd(), program.source)),
       tempSource = normalize(p.join(__dirname, 'temp-build')),
       outputPath = p.join(p.isAbsolute(program.output) ? program.output : p.join(process.cwd(), program.output), p.basename(source) + '.zip'),
-      output = fs.createWriteStream(outputPath),
+
       log = msg => {
         if (!program.quiet) {
           winston.info(msg)
         }
+      },
+
+      // The magic
+      buildItYo = () => {
+        // 2. Copy to temp directory
+        fs.copy(source, tempSource, err => {
+          if (err)
+            return winston.error('Error copying source directory:', err)
+
+          log('Copied source directory to temporary directory')
+
+          // 3. Read the temp directory and only add allowed files
+          fs.readdir(tempSource, (err, files) => {
+            if (err)
+              return winston.error('Error reading source directory:', err)
+
+            each(files, (file, cb) => {
+              if (allowedItems.indexOf(file.toLowerCase()) === -1) {
+                fs.remove(p.join(tempSource, file), err => {
+                  if (err)
+                    return cb(`Error deleting ${file}:`, err)
+
+                  log(`Cleaned ${file} from temporary directory`)
+                  cb()
+                })
+              } else {
+                cb()
+              }
+            }, err => {
+              if (err)
+                return winston.error(err)
+
+              log('Finished sanitizing temporary directory')
+
+              // 4. Pipe to output
+              archive.pipe(output)
+              archive.directory(tempSource, false).finalize()
+            })
+          })
+        })
       }
+
+let output
 
 log(`Starting zip of ${source}`)
 
-output.on('close', () => {
-  log(`Zip file created at ${outputPath}`)
+// 1. Remove the previous build if it exists
+fs.remove(outputPath, () => {
+  log('Removed old zip file')
 
-  fs.remove(tempSource, err => {
-    if (err)
-      return winston.error(`Error deleting ${tempSource}:`, err)
+  output = fs.createWriteStream(outputPath)
 
-    log('Removed temporary directory')
-    log('Build finished!')
+  output.on('close', () => {
+    log(`Zip file created at ${outputPath}`)
+
+    // 5. Delete the temp directory
+    fs.remove(tempSource, err => {
+      if (err)
+        return winston.error(`Error deleting ${tempSource}:`, err)
+
+      log('Removed temporary directory')
+      log('Build finished!')
+    })
   })
-})
 
-output.on('error', err => {
-  winston.error('Failed creating zip file:', err)
+  output.on('error', err => {
+    winston.error('Failed creating zip file:', err)
+  })
+
+  // Build it on next tick
+  process.nextTick(buildItYo)
 })
 
 archive.on('entry', file => {
   log(`Added entry ${file.name}`)
-})
-
-fs.copy(source, tempSource, err => {
-  if (err)
-    return winston.error('Error copying source directory:', err)
-
-  log('Copied source directory to temporary directory')
-
-  fs.readdir(tempSource, (err, files) => {
-    if (err)
-      return winston.error('Error reading source directory:', err)
-
-    each(files, (file, cb) => {
-      if (allowedItems.indexOf(file.toLowerCase()) === -1) {
-        fs.remove(p.join(tempSource, file), err => {
-          if (err)
-            return cb(`Error deleting ${file}:`, err)
-
-          log(`Cleaned ${file} from temporary directory`)
-          cb()
-        })
-      } else {
-        cb()
-      }
-    }, err => {
-      if (err)
-        return winston.error(err)
-
-      log('Finished sanitizing temporary directory')
-
-      archive.pipe(output)
-      archive.directory(tempSource, false).finalize()
-    })
-  })
 })
