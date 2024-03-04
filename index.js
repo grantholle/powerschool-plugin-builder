5f18ceaf38764efcebf8e04e3e300c65ff811dc2
Grant Holle <hollegrant@gmail.com>
2024-03-04 15:26:29 -0600

    Use the OS temp directory for build directory

--- index.js
+++ index.js
@@ -4,11 +4,12 @@
 const program = require('commander')
 const absolutePath = require('./lib/absolutePath')
 const build = require('./lib/build')
+const os = require('os')
 
 program.version(require('./package.json').version)
   .option('-s, --source [path]', 'The path to the plugin directory. If none is given, assumes your current working directory.', absolutePath, process.cwd())
   .option('-o, --output [path]', 'The path to the output directory. If none is given, assumes your current working directory.', absolutePath, process.cwd())
-  .option('-b, --build [path]', 'The path to the temporary build directory. If none is given, assumes your current working directory.', absolutePath, process.cwd())
+  .option('-b, --build [path]', 'The path to the temporary build directory. If none is given, use your OS temp directory.', absolutePath, os.tmpdir())
   .option('-i, --increment [level]', 'Increment the version found `plugin.xml` by release type: major, minor, patch.')
   .option('-q, --quiet', 'Do not log messages')
   .parse(process.argv)
